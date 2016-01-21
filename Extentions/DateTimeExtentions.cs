9d230d792c7440afa4f9c8962503bb3a188da5d2
Geo3ngel <GeoEngel.z@gmail.com>
2016-01-20 18:08:06 -0800

    Added useful exensions

---
+++ Extentions/DateTimeExtentions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using GregorianCalendar = Java.Util.GregorianCalendar;
+
+namespace ScoutingModels.Extentions
+{
+    public static class DateTimeExtentions
+    {
+        public static int GetWeekNumber(this DateTime date)
+        {
+            return GetWeekNumber(date, CultureInfo.CurrentCulture);
+        }
+
+        public static int GetWeekNumber(this DateTime date, CultureInfo culture)
+        {
+            return culture.Calendar.GetWeekOfYear(date,
+                culture.DateTimeFormat.CalendarWeekRule,
+                culture.DateTimeFormat.FirstDayOfWeek);
+        }
+
+        public static int GetWeekNumberOfMonth(this DateTime date)
+        {
+            return GetWeekNumberOfMonth(date, CultureInfo.CurrentCulture);
+        }
+
+        public static int GetWeekNumberOfMonth(this DateTime date, CultureInfo culture)
+        {
+            return date.GetWeekNumber(culture)
+                - new DateTime(date.Year, date.Month, 0).
+            GetWeekNumber(culture);
+        }
+    }
+}
